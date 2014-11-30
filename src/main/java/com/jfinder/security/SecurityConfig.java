fe923b591dbdfa5df9005bc85854d2f16c07ae3e
Abhinav Seewoosungkur <abhinav.seewoosungkur@gmail.com>
2014-11-29 19:01:07 -0500

    Login working with default spring view.

---
+++ src/main/java/com/jfinder/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.jfinder.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Abhinav on 11/29/2014.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+
+    http.authorizeRequests()
+        .antMatchers("/Administrator/**").access("hasRole('ROLE_ADMIN')")
+        .and().formLogin();
+
+  }
+}
---
+++ src/main/java/com/jfinder/security/SpringSecurityInitializer.java
@@ -0,0 +1,11 @@
+package com.jfinder.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Autoloads the Spring Security Filter Chain
+ */
+public class SpringSecurityInitializer extends
+                                       AbstractSecurityWebApplicationInitializer {
+
+}
