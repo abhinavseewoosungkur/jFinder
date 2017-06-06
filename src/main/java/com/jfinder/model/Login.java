4e3e9c03029b26aa2f7665ddfdb37ac4ae84e5f6
Abhinav Seewoosungkur <abhinav.seewoosungkur@gmail.com>
2017-06-06 01:52:50 -0400

    Add User Authentication models and db password

---
+++ src/main/java/com/jfinder/model/Login.java
@@ -0,0 +1,52 @@
+package com.jfinder.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "login")
+public class Login {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String username;
+    private String password;
+    private boolean enabled;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
